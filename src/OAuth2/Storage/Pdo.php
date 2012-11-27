c94f0b24279208d050d9f0e8a60cc33561b54e15
Marco Amado <mjamado@dreamsincode.com>
2012-11-27 14:03:09 +0000

    Update src/OAuth2/Storage/Pdo.php
    Better PDO usage (bindParam in several places, including some really insecure ones) and more user table related customization (most of the time, the user table is already done for the platform).

--- src/OAuth2/Storage/Pdo.php
+++ src/OAuth2/Storage/Pdo.php
@@ -36,13 +36,17 @@ class OAuth2_Storage_Pdo implements OAuth2_Storage_AuthorizationCodeInterface,
             'token_table_name' => 'oauth_access_tokens',
             'code_table_name' => 'oauth_authorization_codes',
             'user_table_name' => 'oauth_users',
+            'user_id_as_int' => false,
+            'username_field' => 'username',
+    		'password_field' => 'password',
         ), $config);
     }
 
     /* ClientCredentialsInterface */
     public function checkClientCredentials($client_id, $client_secret = null)
     {
-        $stmt = $this->db->prepare(sprintf('SELECT * from %s where client_id = "%s"', $this->config['client_table_name'], $client_id));
+        $stmt = $this->db->prepare('SELECT * FROM ' . $this->config['client_table_name'] . ' WHERE client_id = :client_id');
+    	$stmt->bindParam(':client_id', $client_id, PDO::PARAM_STR);
         $stmt->execute();
         $result = $stmt->fetch();
 
@@ -52,7 +56,8 @@ class OAuth2_Storage_Pdo implements OAuth2_Storage_AuthorizationCodeInterface,
 
     public function getClientDetails($client_id)
     {
-        $stmt = $this->db->prepare(sprintf('SELECT * from %s where client_id = "%s"', $this->config['client_table_name'], $client_id));
+        $stmt = $this->db->prepare('SELECT * FROM ' . $this->config['client_table_name'] . ' WHERE client_id = :client_id');
+    	$stmt->bindParam(':client_id', $client_id, PDO::PARAM_STR);
         $stmt->execute();
 
         return $stmt->fetch();
@@ -72,7 +77,8 @@ class OAuth2_Storage_Pdo implements OAuth2_Storage_AuthorizationCodeInterface,
     /* AccessTokenInterface */
     public function getAccessToken($access_token)
     {
-        $stmt = $this->db->prepare(sprintf('SELECT * from %s where access_token = "%s"', $this->config['token_table_name'], $access_token));
+        $stmt = $this->db->prepare('SELECT * FROM ' . $this->config['token_table_name'] . ' WHERE access_token = :access_token');
+    	$stmt->bindParam(':access_token', $access_token, PDO::PARAM_STR);
 
         $token = $stmt->execute();
         if ($token = $stmt->fetch()) {
@@ -90,17 +96,25 @@ class OAuth2_Storage_Pdo implements OAuth2_Storage_AuthorizationCodeInterface,
 
         // if it exists, update it.
         if ($this->getAccessToken($access_token)) {
-            $stmt = $this->db->prepare(sprintf('UPDATE %s SET client_id=:client_id, expires=:expires, user_id=:user_id, scope=:scope where access_token=:access_token', $this->config['token_table_name']));
+            $stmt = $this->db->prepare('UPDATE ' . $this->config['token_table_name'] . ' SET client_id=:client_id, expires=:expires, user_id=:user_id, scope=:scope WHERE access_token=:access_token');
         } else {
-            $stmt = $this->db->prepare(sprintf('INSERT INTO %s (access_token, client_id, expires, user_id, scope) VALUES (:access_token, :client_id, :expires, :user_id, :scope)', $this->config['token_table_name']));
+            $stmt = $this->db->prepare('INSERT INTO ' . $this->config['token_table_name'] . ' (access_token, client_id, expires, user_id, scope) VALUES (:access_token, :client_id, :expires, :user_id, :scope)');
         }
-        return $stmt->execute(compact('access_token', 'client_id', 'user_id', 'expires', 'scope'));
+
+    	$stmt->bindParam(':access_token', $access_token, PDO::PARAM_STR);
+		$stmt->bindParam(':client_id', $client_id, PDO::PARAM_STR);
+		$stmt->bindParam(':user_id', $user_id, $this->config['user_id_as_int'] ? PDO::PARAM_INT : PDO::PARAM_STR);
+		$stmt->bindParam(':expires', $expires, PDO::PARAM_STR);
+		$stmt->bindParam(':scope', $scope, PDO::PARAM_STR);
+
+        return $stmt->execute();
     }
 
     /* AuthorizationCodeInterface */
     public function getAuthorizationCode($code)
     {
-        $stmt = $this->db->prepare(sprintf('SELECT * from %s where authorization_code = "%s"', $this->config['code_table_name'], $code));
+        $stmt = $this->db->prepare('SELECT * FROM ' . $this->config['code_table_name'] . ' WHERE authorization_code = :authorization_code');
+    	$stmt->bindParam(':authorization_code', $code, PDO::PARAM_STR);
         $stmt->execute();
 
         if ($code = $stmt->fetch()) {
@@ -118,11 +132,19 @@ class OAuth2_Storage_Pdo implements OAuth2_Storage_AuthorizationCodeInterface,
 
         // if it exists, update it.
         if ($this->getAuthorizationCode($code)) {
-            $stmt = $this->db->prepare($sql = sprintf('UPDATE %s SET client_id=:client_id, user_id=:user_id, redirect_uri=:redirect_uri, expires=:expires, scope=:scope where authorization_code=:code', $this->config['code_table_name']));
+            $stmt = $this->db->prepare('UPDATE ' . $this->config['code_table_name'] . ' SET client_id=:client_id, user_id=:user_id, redirect_uri=:redirect_uri, expires=:expires, scope=:scope WHERE authorization_code=:code');
         } else {
-            $stmt = $this->db->prepare(sprintf('INSERT INTO %s (authorization_code, client_id, user_id, redirect_uri, expires, scope) VALUES (:code, :client_id, :user_id, :redirect_uri, :expires, :scope)', $this->config['code_table_name']));
+            $stmt = $this->db->prepare('INSERT INTO ' . $this->config['code_table_name'] . ' (authorization_code, client_id, user_id, redirect_uri, expires, scope) VALUES (:code, :client_id, :user_id, :redirect_uri, :expires, :scope)');
         }
-        return $stmt->execute(compact('code', 'client_id', 'user_id', 'redirect_uri', 'expires', 'scope'));
+
+    	$stmt->bindParam(':code', $code, PDO::PARAM_STR);
+		$stmt->bindParam(':client_id', $client_id, PDO::PARAM_STR);
+		$stmt->bindParam(':user_id', $user_id, $this->config['user_id_as_int'] ? PDO::PARAM_INT : PDO::PARAM_STR);
+		$stmt->bindParam(':redirect_uri', $redirect_uri, PDO::PARAM_STR);
+		$stmt->bindParam(':expires', $expires, PDO::PARAM_STR);
+		$stmt->bindParam(':scope', $scope, PDO::PARAM_STR);
+
+        return $stmt->execute();
     }
 
     /* UserCredentialsInterface */
@@ -142,8 +164,9 @@ class OAuth2_Storage_Pdo implements OAuth2_Storage_AuthorizationCodeInterface,
 
     public function getUser($username)
     {
-        $stmt = $this->db->prepare($sql = sprintf('SELECT * from %s where username=:username', $this->config['user_table_name']));
-        $stmt->execute(array('username' => $username));
+        $stmt = $this->db->prepare('SELECT * FROM ' . $this->config['user_table_name'] . ' WHERE ' . $this->config['username_field'] . '=:username');
+    	$stmt->bindParam(':username', $username);
+        $stmt->execute();
         return $stmt->fetch();
     }
 
@@ -151,10 +174,16 @@ class OAuth2_Storage_Pdo implements OAuth2_Storage_AuthorizationCodeInterface,
     {
         // if it exists, update it.
         if ($this->getUser($username)) {
-            $stmt = $this->db->prepare($sql = sprintf('UPDATE %s SET username=:username, password=:password, first_name=:firstName, last_name=:lastName where username=:username', $this->config['user_table_name']));
+            $stmt = $this->db->prepare('UPDATE ' . $this->config['user_table_name'] . ' SET ' . $this->config['username_field'] . '=:username, ' . $this->config['password_field'] . '=:password, first_name=:firstName, last_name=:lastName WHERE username=:username');
         } else {
-            $stmt = $this->db->prepare(sprintf('INSERT INTO %s (username, password, first_name, last_name) VALUES (:username, :password, :firstName, :lastName)', $this->config['user_table_name']));
+            $stmt = $this->db->prepare('INSERT INTO ' . $this->config['user_table_name'] . ' (' . $this->config['username_field'] . ', ' . $this->config['password_field'] . ', first_name, last_name) VALUES (:username, :password, :firstName, :lastName)');
         }
-        return $stmt->execute(compact('username', 'password', 'firstName', 'lastName'));
+
+    	$stmt->bindParam(':username', $username);
+		$stmt->bindParam(':password', $password);
+		$stmt->bindParam(':firstName', $firstName);
+		$stmt->bindParam(':lastName', $lastName);
+
+        return $stmt->execute();
     }
 }
