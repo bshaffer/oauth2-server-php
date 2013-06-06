6ecebeaea9d74c2b6477b1234588937ba1734a8e
Brent Shaffer <bshafs@gmail.com>
2013-06-06 13:13:15 -0600

    removes check for 5.2

--- test/OAuth2/ServerTest.php
+++ test/OAuth2/ServerTest.php
@@ -160,11 +160,6 @@ class ServerTest extends \PHPUnit_Framework_TestCase
 
     public function testAddingStorageWithValidKeyOnlySetsThatKey()
     {
-        if (version_compare(phpversion(), '5.3', '<')) {
-            // cannot run this test in 5.2
-            return;
-        }
-
         $server = new Server();
         $server->addStorage($this->getMock('OAuth2\Storage\Memory'), 'access_token');
 
