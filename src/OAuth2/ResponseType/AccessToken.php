2f74b2be17ea5a0cc2c396d7ed8d1d568ec277df
Brent Shaffer <bshafs@gmail.com>
2013-09-06 23:14:22 -0600

    fixes #227 - squelches open basedir restriction error

--- src/OAuth2/ResponseType/AccessToken.php
+++ src/OAuth2/ResponseType/AccessToken.php
@@ -102,7 +102,7 @@ class AccessToken implements AccessTokenInterface
     protected function generateAccessToken()
     {
         $tokenLen = 40;
-        if (file_exists('/dev/urandom')) { // Get 100 bytes of random data
+        if (@file_exists('/dev/urandom')) { // Get 100 bytes of random data
             $randomData = file_get_contents('/dev/urandom', false, null, 0, 100) . uniqid(mt_rand(), true);
         } else {
             $randomData = mt_rand() . mt_rand() . mt_rand() . mt_rand() . microtime(true) . uniqid(mt_rand(), true);
