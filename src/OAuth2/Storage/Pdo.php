766870df1fca01248b8072915b47e0d0f5a0c1da
Roman Colohanin <rkolohanin@zuzmic.info>
2013-09-20 09:56:00 +0400

    Allowing to set Postgresql as storage. - PDOException with message SQLSTATE[42703]: Undefined column: 7 ERROR:  column "default" does not exist

--- src/OAuth2/Storage/Pdo.php
+++ src/OAuth2/Storage/Pdo.php
@@ -267,8 +267,8 @@ class Pdo implements AuthorizationCodeInterface,
     public function scopeExists($scope, $client_id = null)
     {
         $scope = explode(' ', $scope);
-        $stmt = $this->db->prepare($sql = sprintf('SELECT scope FROM %s WHERE type="supported" AND (client_id=:client_id OR client_id IS NULL) ORDER BY client_id IS NOT NULL DESC', $this->config['scope_table']));
-        $stmt->execute(compact('client_id'));
+        $stmt = $this->db->prepare($sql = sprintf('SELECT scope FROM %s WHERE type=:type AND (client_id=:client_id OR client_id IS NULL) ORDER BY client_id IS NOT NULL DESC', $this->config['scope_table']));
+        $stmt->execute(array('client_id' => $client_id, 'type'=> 'supported'));
 
         if ($result = $stmt->fetch()) {
             $supportedScope = explode(' ', $result['scope']);
@@ -281,8 +281,8 @@ class Pdo implements AuthorizationCodeInterface,
 
     public function getDefaultScope($client_id = null)
     {
-        $stmt = $this->db->prepare($sql = sprintf('SELECT scope FROM %s WHERE type="default" AND (client_id=:client_id OR client_id IS NULL) ORDER BY client_id IS NOT NULL DESC', $this->config['scope_table']));
-        $stmt->execute(compact('client_id'));
+        $stmt = $this->db->prepare($sql = sprintf('SELECT scope FROM %s WHERE type=:type AND (client_id=:client_id OR client_id IS NULL) ORDER BY client_id IS NOT NULL DESC', $this->config['scope_table']));
+        $stmt->execute(array('client_id' => $client_id, 'type' => 'default'));
 
         if ($result = $stmt->fetch()) {
             return $result['scope'];
