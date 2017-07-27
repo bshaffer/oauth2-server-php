052d7173bc2d4fcd6eccd03418ad75888c16e779
Michael Neumüller <mn@dig.at>
2017-07-27 14:10:43 +0200

    Fix buildUri for URIs with fragments
    Output query part after fragment part otherwise redirect URIs like https://www.example.com/#/oauth2 don't work

--- src/OAuth2/Controller/AuthorizeController.php
+++ src/OAuth2/Controller/AuthorizeController.php
@@ -376,8 +376,8 @@ class AuthorizeController implements AuthorizeControllerInterface
             . ((isset($parse_url["host"])) ? $parse_url["host"] : "")
             . ((isset($parse_url["port"])) ? ":" . $parse_url["port"] : "")
             . ((isset($parse_url["path"])) ? $parse_url["path"] : "")
-            . ((isset($parse_url["query"]) && !empty($parse_url['query'])) ? "?" . $parse_url["query"] : "")
             . ((isset($parse_url["fragment"])) ? "#" . $parse_url["fragment"] : "")
+            . ((isset($parse_url["query"]) && !empty($parse_url['query'])) ? "?" . $parse_url["query"] : "")
         ;
     }
 
