9fb130c73b3b305b1d5d50ca13d60066d8dd6544
Andrey Filippov <afilippov2009@mail.ru>
2017-09-18 10:13:40 +0400

    added default value to server config

--- src/OAuth2/Server.php
+++ src/OAuth2/Server.php
@@ -139,6 +139,7 @@ class Server implements ResourceControllerInterface,
         // merge all config values.  These get passed to our controller objects
         $this->config = array_merge(array(
             'use_jwt_access_tokens'        => false,
+            'jwt_extra_payload'        => null,
             'store_encrypted_token_string' => true,
             'use_openid_connect'       => false,
             'id_lifetime'              => 3600,
