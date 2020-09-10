2a60bab7e172500c6499305232f6b2303dfca738
Melinda Serven <melinda.serven@acquia.com>
2020-09-10 09:18:36 -0700

    add backtrace logging

--- src/OAuth2/ClientAssertionType/HttpBasic.php
+++ src/OAuth2/ClientAssertionType/HttpBasic.php
@@ -71,6 +71,7 @@ class HttpBasic implements ClientAssertionTypeInterface
                 return false;
             }
         } elseif ($this->storage->checkClientCredentials($clientData['client_id'], $clientData['client_secret']) === false) {
+            error_log(print_r(debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT), true));
             $response->setError(400, 'invalid_client', 'The client credentials are invalid');
 
             return false;
