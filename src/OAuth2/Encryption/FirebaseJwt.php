19b5d350d5fe63c810cbda97e907ebe47d51ff41
Jan-Sverre Riksfjord <jasverix@gmail.com>
2023-02-21 13:36:09 +0100

    upgrade firebase/jwt

--- src/OAuth2/Encryption/FirebaseJwt.php
+++ src/OAuth2/Encryption/FirebaseJwt.php
@@ -2,6 +2,8 @@
 
 namespace OAuth2\Encryption;
 
+use Firebase\JWT\Key;
+
 /**
  * Bridge file to use the firebase/php-jwt package for JWT encoding and decoding.
  * @author Francis Chuang <francis.chuang@gmail.com>
@@ -10,26 +12,36 @@ class FirebaseJwt implements EncryptionInterface
 {
     public function __construct()
     {
-        if (!class_exists('\JWT')) {
+        if (!class_exists('\Firebase\JWT\JWT')) {
             throw new \ErrorException('firebase/php-jwt must be installed to use this feature. You can do this by running "composer require firebase/php-jwt"');
         }
     }
 
     public function encode($payload, $key, $alg = 'HS256', $keyId = null)
     {
-        return \JWT::encode($payload, $key, $alg, $keyId);
+        return \Firebase\JWT\JWT::encode($payload, $key, $alg, $keyId);
     }
 
     public function decode($jwt, $key = null, $allowedAlgorithms = null)
     {
         try {
-
             //Maintain BC: Do not verify if no algorithms are passed in.
             if (!$allowedAlgorithms) {
-                $key = null;
+                $tks = \explode('.', $jwt);
+                if (\count($tks) === 3) {
+                    [$headb64] = $tks;
+                    $headerRaw = \Firebase\JWT\JWT::urlsafeB64Decode($headb64);
+                    if (($header = \Firebase\JWT\JWT::jsonDecode($headerRaw))) {
+                        $key = new Key($key, $header->alg);
+                    }
+                }
+            } elseif(is_array($allowedAlgorithms)) {
+                $key = new Key($key, $allowedAlgorithms[0]);
+            } else {
+                $key = new Key($key, $allowedAlgorithms);
             }
 
-            return (array)\JWT::decode($jwt, $key, $allowedAlgorithms);
+            return (array)\Firebase\JWT\JWT::decode($jwt, $key);
         } catch (\Exception $e) {
             return false;
         }
@@ -37,11 +49,11 @@ class FirebaseJwt implements EncryptionInterface
 
     public function urlSafeB64Encode($data)
     {
-        return \JWT::urlsafeB64Encode($data);
+        return \Firebase\JWT\JWT::urlsafeB64Encode($data);
     }
 
     public function urlSafeB64Decode($b64)
     {
-        return \JWT::urlsafeB64Decode($b64);
+        return \Firebase\JWT\JWT::urlsafeB64Decode($b64);
     }
 }
