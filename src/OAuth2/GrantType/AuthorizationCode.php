112d4d97032668a64ecd2b067e0454aff460376a
Jan Kalina <jan.kalina@fantom.foundation>
2023-05-20 18:59:38 +0200

    Fix formatting by review notes

--- src/OAuth2/GrantType/AuthorizationCode.php
+++ src/OAuth2/GrantType/AuthorizationCode.php
@@ -85,41 +85,38 @@ class AuthorizationCode implements GrantTypeInterface
         }
 
         if (isset($authCode['code_challenge']) && $authCode['code_challenge']) {
-          if (!($code_verifier = $request->request('code_verifier'))) {
-            $response->setError(400, 'code_verifier_missing', "The PKCE code verifier parameter is required.");
+            if (!($code_verifier = $request->request('code_verifier'))) {
+                $response->setError(400, 'code_verifier_missing', "The PKCE code verifier parameter is required.");
 
-            return false;
-          }
-          // Validate code_verifier according to RFC-7636
-          // @see: https://tools.ietf.org/html/rfc7636#section-4.1
-          elseif (preg_match('/^[A-Za-z0-9-._~]{43,128}$/', $code_verifier) !== 1) {
-            $response->setError(400, 'code_verifier_invalid', "The PKCE code verifier parameter is invalid.");
+                return false;
+            }
+            // Validate code_verifier according to RFC-7636
+            // @see: https://tools.ietf.org/html/rfc7636#section-4.1
+            if (preg_match('/^[A-Za-z0-9-._~]{43,128}$/', $code_verifier) !== 1) {
+                $response->setError(400, 'code_verifier_invalid', "The PKCE code verifier parameter is invalid.");
 
-            return false;
-          }
-          else {
+                return false;
+            }
             $code_verifier = $request->request('code_verifier');
             switch ($authCode['code_challenge_method']) {
-              case 'S256':
-                $code_verifier_hashed = strtr(rtrim(base64_encode(hash('sha256', $code_verifier, true)), '='), '+/', '-_');
-                break;
+                case 'S256':
+                    $code_verifier_hashed = strtr(rtrim(base64_encode(hash('sha256', $code_verifier, true)), '='), '+/', '-_');
+                    break;
 
-              case 'plain':
-                $code_verifier_hashed = $code_verifier;
-                break;
+                case 'plain':
+                    $code_verifier_hashed = $code_verifier;
+                    break;
 
-              default:
-                $response->setError(400, 'code_challenge_method_invalid', "Unknown PKCE code challenge method.");
+                default:
+                    $response->setError(400, 'code_challenge_method_invalid', "Unknown PKCE code challenge method.");
 
                 return FALSE;
             }
-            // @TODO: use hash_equals in recent versions of PHP.
             if ($code_verifier_hashed !== $authCode['code_challenge']) {
-              $response->setError(400, 'code_verifier_mismatch', "The PKCE code verifier parameter does not match the code challenge.");
+                $response->setError(400, 'code_verifier_mismatch', "The PKCE code verifier parameter does not match the code challenge.");
 
-              return FALSE;
+                return FALSE;
             }
-          }
         }
 
         if (!isset($authCode['code'])) {
