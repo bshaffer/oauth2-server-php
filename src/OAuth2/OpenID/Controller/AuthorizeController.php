9d003b2767c7726476264c8e21d837cd12ef401f
vampirefrog <vampirefrog@users.noreply.github.com>
2023-10-10 20:58:47 +0300

    Update AuthorizeController.php
    forgot to pass $userClaims

--- src/OAuth2/OpenID/Controller/AuthorizeController.php
+++ src/OAuth2/OpenID/Controller/AuthorizeController.php
@@ -83,7 +83,7 @@ class AuthorizeController extends BaseAuthorizeController implements AuthorizeCo
         // Generate an id token if needed.
         if ($this->needsIdToken($this->getScope()) && $this->getResponseType() == self::RESPONSE_TYPE_AUTHORIZATION_CODE) {
             $userClaims = $this->userClaimsStorage->getUserClaims($user_id, $params['scope']);
-            $params['id_token'] = $this->responseTypes['id_token']->createIdToken($this->getClientId(), $user_id, $this->nonce);
+            $params['id_token'] = $this->responseTypes['id_token']->createIdToken($this->getClientId(), $user_id, $this->nonce, $userClaims);
         }
 
         // add the nonce to return with the redirect URI
